0c494bd49c0d5bcde90d83275753b12da52acab3
Fernando <156912120+FerNegreiro@users.noreply.github.com>
2025-10-02 08:23:28 -0300

    Update newave_loader.py

--- loaders/newave_loader.py
+++ loaders/newave_loader.py
@@ -5,6 +5,6 @@ class NewaveLoader:
         self.file_path = file_path
 
     def load_and_process(self) -> pd.Series:
-        # Lógica SIMULADA. A implementação real usaria a biblioteca 'inewave'.
+       
         cmo_df = pd.read_csv(self.file_path, parse_dates=['date'], index_col='date')
         return cmo_df['cmo_se'].asfreq('MS').dropna()
